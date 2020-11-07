09ce642fe93451b3576351fa50fe15c7828bd55f
Jelle Breuer <jelle@breuclan.nl>
2020-11-07 16:36:35 +0100

    Docker config values

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'config' => [
+            'driver' => 'local',
+            'root' => '/config',
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'sync'),
+    'default' => env('QUEUE_CONNECTION', 'database'),
 
     /*
     |--------------------------------------------------------------------------
@@ -82,7 +82,7 @@ return [
 
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
-        'database' => env('DB_CONNECTION', 'mysql'),
+        'database' => env('DB_CONNECTION', 'sqlite'),
         'table' => 'failed_jobs',
     ],
 
