bf8ffc15102ac5bed37fefdb87650cfdebf105d5
Jelle Breuer <jelle@breuclan.nl>
2020-11-07 17:42:25 +0100

    Log outside container

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', 'daily'),
 
     /*
     |--------------------------------------------------------------------------
@@ -49,7 +49,7 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => '/config/logs/cdarr.log',
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
         ],
