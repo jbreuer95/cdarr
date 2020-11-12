6cd4d45c236f118ec9c81827ba25720489f49dec
Jelle Breuer <jelle@breuclan.nl>
2020-11-12 01:27:47 +0100

    Small nav icon styling fix

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -14,12 +14,12 @@
             value=""
         >
     </div>
-    <div class="flex flex-grow justify-end space-x-3 mx-4">
-        <a href="/" class="hidden xs:block w-5 h-5 hover:text-gray-700">
+    <div class="flex flex-1 justify-end mx-4">
+        <a href="/" class="hidden xs:block w-5 h-5 hover:text-gray-700 ml-3">
             <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
         </a>
-        <a href="/" class="hidden xs:block w-5 h-5 hover:text-gray-700">
-            <svg class="hidden xs:block w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+        <a href="/" class="hidden xs:block w-5 h-5 hover:text-gray-700 ml-3">
+            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
         </a>
     </div>
 </nav>
