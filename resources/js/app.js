6da6af55da03a61bbee4de9fcdd99fd2de6ef948
Jelle Breuer <jelle@breuclan.nl>
2022-06-12 00:59:07 +0200

    Fix2 running on port

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,8 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: 'local',
     cluster: 'local',
-    wsHost: window.location.host,
+    wsHost: window.location.hostname,
+    wsPort: window.location.port,
     forceTLS: false,
     disableStats: true,
     enabledTransports: ['ws', 'wss']
