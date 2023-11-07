65b7ab9348b34f0375641db0d0f5289cffcf008e
Jelle Breuer <jelle@breuclan.nl>
2023-11-07 20:29:14 +0100

    add filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,18 @@ return [
             'throw' => false,
         ],
 
+        'movies' => [
+            'driver' => 'local',
+            'root' => '/movies',
+            'throw' => false,
+        ],
+
+        'tv' => [
+            'driver' => 'local',
+            'root' => '/tv',
+            'throw' => false,
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
